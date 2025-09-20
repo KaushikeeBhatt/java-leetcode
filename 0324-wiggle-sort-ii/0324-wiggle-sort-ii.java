24854e1d342fb68ea53b6ec905dc6668f52cae47
Kaushikee Bhatt <kaushikeebhatt4@gmail.com>
2025-09-21 02:57:34 +0530

    Time: 6 ms (74.91%), Space: 48.1 MB (12.76%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -1,16 +1,12 @@
-import java.util.Arrays;
-
 class Solution {
     public void wiggleSort(int[] nums) {
-        int n = nums.length;
-        int[] sorted = nums.clone();
+        int n=nums.length;
+        int[] sorted=nums.clone();
+        int left=(n-1)/2;
+        int right=n-1;
         Arrays.sort(sorted);
-
-        int left = (n - 1) / 2; // end of smaller half
-        int right = n - 1;      // end of larger half
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = (i % 2 == 0) ? sorted[left--] : sorted[right--];
+        for(int i=0;i<n;i++){
+            nums[i]=(i%2==0)?sorted[left--]: sorted[right--];
         }
     }
 }
